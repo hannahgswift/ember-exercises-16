d704db5b62baf7401dc35274cf6b829bf84200c6
Hannah Swift <h.graceswift@gmail.com>
2016-07-20 20:43:03 -0500

    New contacts added to list

---
+++ app/controllers/contacts.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  actions: {
+    addContact() {
+      const data = {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          address: this.address,
+          phoneNumber: this.phoneNumber
+        };
+
+        fetch('https://tiny-tn.herokuapp.com/collections/hs-contacts', {
+          headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+          },
+          method: 'post',
+          body: JSON.stringify(data),
+        });
+    }
+  }
+});
