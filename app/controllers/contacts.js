b6d93df37489d1d15970af7070c8da63cc1065b2
Hannah Swift <h.graceswift@gmail.com>
2016-07-20 21:54:31 -0500

    Working on deleting records, not working

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -4,24 +4,40 @@ export default Ember.Controller.extend({
   actions: {
     addContact() {
       const data = {
-          firstName: this.firstName,
-          lastName: this.lastName,
-          address: this.address,
-          phoneNumber: this.phoneNumber
-        };
+        firstName: this.firstName,
+        lastName: this.lastName,
+        address: this.address,
+        phoneNumber: this.phoneNumber
+      };
 
-        fetch('https://tiny-tn.herokuapp.com/collections/hs-contacts', {
-          headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          method: 'post',
-          body: JSON.stringify(data),
-        });
+      fetch('https://tiny-tn.herokuapp.com/collections/hs-contacts', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify(data),
+      });
     },
 
     toggleForm() {
       this.toggleProperty('isShowingForm');
+    },
+
+    deleteContact() {
+      const data = {
+      };
+
+      fetch('https://tiny-tn.herokuapp.com/collections/hs-contacts', {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        method: 'delete'}).then((res) => res.json()).then(() => {
+          var todo = this.get('model');
+          todo.deleteRecord();
+          todo.save();
+        })
     }
   }
 });
