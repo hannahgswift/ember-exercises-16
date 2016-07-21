2904aeceba05866ca896e090cc1bf0fda20cefbb
Hannah Swift <h.graceswift@gmail.com>
2016-07-21 15:49:22 -0500

    Delete contact works

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -24,20 +24,19 @@ export default Ember.Controller.extend({
       this.toggleProperty('isShowingForm');
     },
 
-    deleteContact() {
-      const data = {
-      };
-
-      fetch('https://tiny-tn.herokuapp.com/collections/hs-contacts', {
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json'
-        },
-        method: 'delete'}).then((res) => res.json()).then(() => {
-          var todo = this.get('model');
-          todo.deleteRecord();
-          todo.save();
+    deleteContact(kitty) {
+      if (confirm('Are you sure you want to delete?')) {
+        fetch('https://tiny-tn.herokuapp.com/collections/hs-contacts' + kitty._id, {
+          method: 'Delete',
+        }).then(() => {
+          const updatedList = this.model.filter((item) => {
+            return item._id !== kitty._id;
+          });
+          this.set('model', updatedList);
         })
-    }
+      }
+    },
+
+
   }
 });
